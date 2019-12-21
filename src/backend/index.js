7934aa73381da0872e5d1286144aeee22342ae25
Yakov Kovalev <yataw@ya.ru>
2019-12-21 23:48:52 +0300

    #heroku fixBuild 4# clean project

--- src/backend/index.js
+++ src/backend/index.js
@@ -4,12 +4,8 @@ const path = require('path');
 const logger = require('morgan');
 
 // TODO add favicon
-// const favicon = require('serve-favicon');
 const log = require('./logger')(module);
-// const methodOverride = require('method-override');
-// const session = require('express-session');
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
 
 const app = express();
 const server = require('http').createServer(app);
@@ -24,11 +20,8 @@ server.listen(port, () => console.log(`listening on ${port}`));
 
 app.use(logger('dev'));
 app.use(bodyParser.json()); // тело запроса (POST body) помещает в req.body
-app.use(cookieParser()); // куки парсит в req.cookies
 app.use(require('./middleware/sendHttpError'));
 
 // TODO add favicon
 // app.use(favicon('public/icons/favicon.ico')); // возвращает favicon, если перешли по /favicon.ico
 app.use(express.static(path.join(__dirname, '../../build'))); // роут на статические (неизменяемые) ресурсы из директории public
-
-// app.use(express.static(path.join(__dirname, '../client/build')));
